8f912ebbdfcf804155d2096bdf33ab4f1b834994
김민재 <poporesion@naver.com>
2026-06-29 12:30:10 +0900

    Delete horses directory

--- horses/HORSE/dto/member.py
+++
@@ -1,8 +0,0 @@
-class MemberDTO:
-    def __init__(self, user_id, password, name, money, is_admin=False):
-        self.user_id = user_id
-        self.password = password
-        self.name = name
-        self.money = money
-        self.my_horses = []
-        self.is_admin = is_admin
